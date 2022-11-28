022de28218e5e233edabb386e4978d2579f4ddef
TheCodingFreakj <pallavidapriya75@gmail.com>
2022-11-29 02:03:34 +0530

    shortcodes added

--- public/js/main.js
+++ public/js/main.js
@@ -167,6 +167,41 @@
       }
     }
   }
-  
+  const tabs = document.querySelectorAll('[data-tab-value]')
+  const tabInfos = document.querySelectorAll('[data-tab-info]')
+
+  tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+          const target = document
+              .querySelector(tab.dataset.tabValue);
+
+          tabInfos.forEach(tabInfo => {
+              tabInfo.classList.remove('active')
+          })
+          target.classList.add('active');
+      })
+  })
+
+  const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
+
+
 
 })(jQuery);
