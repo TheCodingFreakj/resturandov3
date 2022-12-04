58b6fcae72537f7e27644a1becdaecab67b4e499
TheCodingFreakj <pallavidapriya75@gmail.com>
2022-12-04 11:04:15 +0530

    done

--- app/models/reservation.model.js
+++ app/models/reservation.model.js
@@ -9,22 +9,26 @@ class Reservations {
     this.date = reservation.date;
     this.phone = reservation.phone;
     this.guests = reservation.guests;
-    this.time = reservation.time;
+    this.time = STR_TO_DATE(reservation.time, "%m/%d/%Y");
   }
   static create(newReservation, result) {
-    connection.query("INSERT INTO reservations SET ?", newReservation, (err, res) => {
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
+    connection.query(
+      "INSERT INTO reservations SET ?",
+      newReservation,
+      (err, res) => {
+        if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
 
-      console.log("created newReservation: ", {
-        id: res.insertId,
-        ...newReservation,
-      });
-      result(null, { id: res.insertId, ...newReservation });
-    });
+        console.log("created newReservation: ", {
+          id: res.insertId,
+          ...newReservation,
+        });
+        result(null, { id: res.insertId, ...newReservation });
+      }
+    );
   }
 }
 export default Reservations;
